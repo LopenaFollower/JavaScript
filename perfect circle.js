3358b2b29da426dd048af1b50198def95e440d77
LopenaFollower <115658149+LopenaFollower@users.noreply.github.com>
2024-02-24 11:56:44 +0800

    Add files via upload

--- perfect circle.js
+++ perfect circle.js
@@ -1,31 +1,6 @@
 javascript:(function(){
-	let d=document.querySelector("main div"),
-		inp=document.createElement("input"),
-		style=inp.style,
-		i=null;
-	style.position="fixed";
-	style.top="5px";
-	style.left=window.innerWidth/3+"px";
-	inp.oninput=function(){
-		if(inp.value<=93)return;
-		clearInterval(i)
-		let s=d.getBoundingClientRect(),
-			cx=s.width/2+s.x,
-			cy=s.height/2+s.y,
-			r=inp.value,
-			e=-1
-		i=setInterval(()=>{
-			if(++e<360){
-				let a=(Math.PI/180)*e,
-					x=Math.round(cx+r*Math.cos(a)),
-					y=Math.round(cy+r*Math.sin(a));
-				d.dispatchEvent(new MouseEvent("mousedown",{clientX:x,clientY:y})),
-				d.dispatchEvent(new MouseEvent("mousemove",{clientX:x,clientY:y}))
-			}else{
-				clearInterval(i)
-				d.dispatchEvent(new MouseEvent("mouseup"))
-			}
-		})
-	}
-	document.body.appendChild(inp)
-})()
+	let d=document.querySelector("main div"),s=d.getBoundingClientRect(),X=s.width/2+s.x,Y=s.height/2+s.y,r=2e3,m=Math,e=(t,c)=>d.dispatchEvent(new MouseEvent(t,c)),i,a;
+	e("mousedown",{clientX:X+r,clientY:Y});
+	for(i=a=0;i<361;i+=90,a=i*m.PI/180)e("mousemove",{clientX:X+r*m.cos(a),clientY:Y+r*m.sin(a)});
+	e("click");
+})();
