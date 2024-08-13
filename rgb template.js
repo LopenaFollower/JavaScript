24524aa7ce95bbdc5f3025b17796978d9b4d00ee
LopenaFollower <115658149+LopenaFollower@users.noreply.github.com>
2024-08-13 21:31:33 +0800

    Update rgb template.js

--- rgb template.js
+++ rgb template.js
@@ -1,9 +1,9 @@
 class RGB{
-	c=[255,0,0];i=0;j=1;
+	c=[255,0,0];i=0;j=1;l=1;
 	constructor(step=1){
 		this.s=this.d=step;
 		const u=()=>{
-			this.l=requestAnimationFrame(u);
+			this.l&&requestAnimationFrame(u);
 			if(this.d<0?this.c[this.i]>0:this.c[this.j]<255)this.c[this.d<0?this.i:this.j]+=this.d;
 			else if(this.s+(this.d=-this.d)){
 				this.i=++this.i%3;
@@ -13,7 +13,7 @@ class RGB{
 		u();
 	}
 	stop(){
-		cancelAnimationFrame(this.l);
+		this.l=0;
 	}
 	get(){
 		return"#"+this.c.map(e=>e.toString(16).padStart(2,0)).join("");
